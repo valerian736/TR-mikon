47be938fa7378e60d082c81cc81b0ebc6a3cc843
Val <47318672+valerian736@users.noreply.github.com>
2025-05-25 16:29:03 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,65 @@
 #include <Wire.h>
 #include <MPU6050.h>
-
-MPU6050 mpu;
-
-const int crashThreshold = 20000; // Adjust this value based on your needs
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+Adafruit_MPU6050 mpu;
+bool crashDetected = false;
+const int crashThreshold = 20; // Adjust this value based on your needs
+MPU6050 mpu6050;
 
 void setup()
 {
+  pinMode(11, OUTPUT);
   Serial.begin(9600);
-  Wire.begin();
-  mpu.initialize();
-
-  if (!mpu.testConnection())
-  {
-    Serial.println("MPU6050 connection failed");
-    while (1)
-      ;
+  
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
   }
-  Serial.println("MPU6050 connection successful");
-  mpu.setXAccelOffset(782);
-  mpu.setYAccelOffset(-831);
-  mpu.setZAccelOffset(1413);
-  mpu.setXGyroOffset(166);
-  mpu.setYGyroOffset(-54);
-  mpu.setZGyroOffset(-70);
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("");
+  delay(100);
+
+  attachInterrupt(digitalPinToInterrupt(2), []() {
+    crashDetected = false; // Reset crash detection on interrupt
+  }, RISING); // Change to FALLING if needed
 }
 
 void loop()
 {
-  int16_t ax, ay, az;
-  mpu.getAcceleration(&ax, &ay, &az);
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-  long totalAcceleration = (long)ax * ax + (long)ay * ay;
+  long totalAcceleration = (float)a.acceleration.x * a.acceleration.x + (float)a.acceleration.y * a.acceleration.y;
   long acceleration = sqrt(totalAcceleration);
   if (acceleration > crashThreshold)
   {
-    Serial.println("Crash detected! \t Total acceleration: " + String(totalAcceleration));
+    Serial.println("Crash detected! \t Total acceleration: " + String(acceleration));
+    crashDetected = true;
+
   }
-  Serial.println("Acceleration: " + String(acceleration));
+  else{
+    Serial.print("Acceleration: " + String(acceleration));
+    Serial.print(" \t X: " + String(a.acceleration.x) + " \t Y: " + String(a.acceleration.y) + " \t Z: " + String(a.acceleration.z));
+    Serial.println();
+  }
+
+  if (crashDetected)
+  {
+    tone(11, 30000); 
+  }
+  else
+  {
+    noTone(11); 
+  }
+
+  
+
   delay(100);
 }
